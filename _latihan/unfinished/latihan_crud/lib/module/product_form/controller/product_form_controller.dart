80455722027a3ec9bba87f2839f80104fa42a774
Deny <denyocr.world@gmail.com>
2023-11-15 16:17:57 +0700

    .

--- _latihan/unfinished/latihan_crud/lib/module/product_form/controller/product_form_controller.dart
+++ _latihan/unfinished/latihan_crud/lib/module/product_form/controller/product_form_controller.dart
@@ -1,23 +1,72 @@
-
 import 'package:flutter/material.dart';
 import 'package:hyper_ui/core.dart';
 import '../view/product_form_view.dart';
 
 class ProductFormController extends State<ProductFormView> {
-    static late ProductFormController instance;
-    late ProductFormView view;
+  static late ProductFormController instance;
+  late ProductFormView view;
 
-    @override
-    void initState() {
-        instance = this;
-        super.initState();
+  @override
+  void initState() {
+    instance = this;
+    /*
+    TODO: 8
+    Tambahkan kode berikut
+    if(isEditMode){
+      photo = widget.item!["photo"];
+      productName = widget.item!["product_name"];
+      price = widget.item!["price"];
+      description = widget.item!["description"];
+    }
+    */
+
+    //TODO: 9
+    // Hot Restart, login lagi
+    // Coba semua fitur Create, Update dan Delete dari Products apakah sudah bekerja?
+    super.initState();
+  }
+
+  @override
+  void dispose() => super.dispose();
+
+  @override
+  Widget build(BuildContext context) => widget.build(context, this);
+
+  //TODO: 5. Buatlah getter isEditMode
+  // bool get isEditMode => widget.item != null;
+
+  //TODO: 6. Buatlah state
+  /*
+    String? photo;
+    String? productName;
+    double? price;
+    String? description;
+  */
+
+  //TODO: 7. Buatlah method save() async
+  /*
+    Tambahkan kode berikut:
+    -----
+    if(isEditMode) {
+      //Edit
+      await  ProductService().update(widget.item!["id"], {
+        "photo": photo!,
+        "productName": productName!,
+        "price": price!,
+        "description": description!,
+      });
+    }
+    else {
+      //Create
+      await ProductService().create({
+        "photo": photo!,
+        "productName": productName!,
+        "price": price!,
+        "description": description!,
+      });
     }
 
-    @override
-    void dispose() => super.dispose();
-
-    @override
-    Widget build(BuildContext context) => widget.build(context, this);
+    Get.back();
+    -----
+  */
 }
-        
-    
--- _latihan/unfinished/latihan_crud/lib/module/product_form/view/product_form_view.dart
+++ _latihan/unfinished/latihan_crud/lib/module/product_form/view/product_form_view.dart
@@ -1,31 +1,65 @@
-
 import 'package:flutter/material.dart';
 import 'package:hyper_ui/core.dart';
 import '../controller/product_form_controller.dart';
 
 class ProductFormView extends StatefulWidget {
-    const ProductFormView({Key? key}) : super(key: key);
+  //TODO: 1. Tambahkan kode berikut
+  /*
+    final Map? item;
+    const ProductFormView({
+      Key? key,
+      this.item,
+    }) : super(key: key);
+  */
 
-    Widget build(context, ProductFormController controller) {
+  Widget build(context, ProductFormController controller) {
     controller.view = this;
 
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         title: const Text("ProductForm"),
         actions: const [],
-        ),
-        body: SingleChildScrollView(
+      ),
+      body: SingleChildScrollView(
         child: Container(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-            children: const [],
-            ),
-        ),
-        ),
-    );
-    }
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: const [
+              /*
+              TODO: 4. Buatlah Form dengan data berikut:
+              q_image_picker    => Photo
+                value: controller.photo,
+                onChanged: (value) {
+                  controller.photo = value;
+                }
 
-    @override
-    State<ProductFormView> createState() => ProductFormController();
+              q_textfield       => Product Name
+                value: controller.productName,
+                onChanged: (value) {
+                  controller.productName = value;
+                }
+
+              q_numberfield     => Price
+                value: controller.price?.toString(),
+                onChanged: (value) {
+                  controller.price = double.tryParse(value) ?? 0;
+                }
+
+              q_memofield       => Description
+                value: controller.description,
+                onChanged: (value) {
+                  controller.description = value;
+                }
+              */
+            ],
+          ),
+        ),
+      ),
+      //TODO: 2. Tambahkan button Save di bottom NavigationBar, q_action_bottom_action
+      //TODO: 3. Panggil method controller.save() ketika tombol Save di klik
+    );
+  }
+
+  @override
+  State<ProductFormView> createState() => ProductFormController();
 }
-    
